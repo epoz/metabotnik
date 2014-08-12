cd390f8a6c5c7e4bb313db03472101283bba5a14
Etienne Posthumus <eposthumus@gmail.com>
2014-08-12 10:17:18 +0200

    Add request context processor

--- src/metabotnik/settings.py
+++ src/metabotnik/settings.py
@@ -87,6 +87,17 @@ AUTHENTICATION_BACKENDS = (
     'metabotnik.auth.DropboxAuthBackend',
 )
 
+TEMPLATE_CONTEXT_PROCESSORS =(
+    "django.contrib.auth.context_processors.auth",
+    "django.core.context_processors.debug",
+    "django.core.context_processors.i18n",
+    "django.core.context_processors.media",
+    "django.core.context_processors.static",
+    "django.core.context_processors.tz",
+    "django.contrib.messages.context_processors.messages",
+    "django.core.context_processors.request"
+)
+
 LOGIN_URL = '/login'
 
 STORAGE_PATH = os.path.join(BASE_DIR, 'storage')
