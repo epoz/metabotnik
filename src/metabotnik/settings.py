a8275d0feea93c17e84967ef621dcad34b275c6c
Etienne Posthumus <eposthumus@gmail.com>
2014-10-11 09:31:56 +0200

    Move database settings to local

--- src/metabotnik/settings.py
+++ src/metabotnik/settings.py
@@ -56,12 +56,12 @@ WSGI_APPLICATION = 'metabotnik.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.6/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.6/topics/i18n/
