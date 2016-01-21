afe616a1d16e3e71e25ab6422157143ee64f2fab
Etienne Posthumus <eposthumus@gmail.com>
2016-01-21 23:29:42 +0100

    use django_markup

--- src/metabotnik/settings.py
+++ src/metabotnik/settings.py
@@ -35,7 +35,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django.contrib.markup',
+    'django_markup',
     'metabotnik',
     'south',
 )
